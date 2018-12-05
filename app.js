a433b1fb1615f59167ab437d9bdcbd453e0a6bb1
JonyTorres <tonyjorres@gmail.com>
2018-12-04 22:30:40 -0500

    Actualizar Json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const argv = require('./config/yargs').argv;
 
 const porHacer = require('./por-hacer/por-hacer');
+const colores = require('colors');
 
 console.log(argv);
 
@@ -10,12 +11,32 @@ let comando = argv._[0];
 
 switch(comando){
     case 'listar':
+        
+        let listado = porHacer.getListado();
+        for(let tarea of listado){
+            console.log('========= Por hacer =========='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado: ',tarea.completado);
+            console.log('=============================='.green);
+        }
 
-        console.log('Mostrar todas las tareas por hacer');
         break;
     case 'crear':
-        let tarea =  porHacer.crear(argv.descripcion);
+        let tarea =  porHacer.crear(comando.descripcion);
         console.log(tarea);
+        break;
+    case 'actualizar':
+        console.log('coroto de parametros');
+        console.log(argv.descripcion);
+        let actualizo = porHacer.actualizar(comando.descripcion, comando.completado);
+        if(actualizo){
+            console.log('actualizo');
+        }
+        else{
+            console.log('No actualizo');
+        }
+        //console.log(tarea);
+        break;
     default:
         console.log('Comando no definido');
 }
--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -10,12 +10,31 @@ const guardarDB = () =>{
         if(err){
             throw new Error('NO se pudo grabar',err);
         }
-        
     });
 
 }
 
+const cargarDB = () =>{
+
+    try{
+        listadoPorHacer = require('../db/data');
+    }
+    catch(err){
+        listadoPorHacer = [];
+    }
+    //console.log(listadoPorHacer);
+}
+
+const getListado = () =>{
+
+    cargarDB();
+    return listadoPorHacer;
+
+} 
+
 const crear = (descripcion) => {
+
+    cargarDB();
 
     let porHacer = {
         descripcion,
@@ -28,7 +47,25 @@ const crear = (descripcion) => {
 
 }
 
+const actualizar = (descripcion, completado) =>{
+    cargarDB();
+    console.log('descripcion', descripcion);
+    let Index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    console.log(listadoPorHacer);
+    console.log(descripcion);
+    console.log(Index);
+    if(Index >= 0){
+        listadoPorHacer[Index].completado = completado;
+        guardarDB();
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 module.exports ={
-    crear
+    crear,
+    getListado,
+    actualizar
 }
