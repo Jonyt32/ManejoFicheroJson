ee531a98db3ec37d92079478725299d6966f4bfd
JonyTorres <tonyjorres@gmail.com>
2018-12-04 23:27:42 -0500

    acortar bloques de comandos

--- config/yargs.js
+++ config/yargs.js
@@ -1,32 +1,25 @@
+const descripcion ={
+    alias: 'd',
+    demand: true,
+    desc: 'Descripción de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea por hacer'
+}
+
 const argv = require('yargs')
             .command('borrar','borra las tareas por hacer',{
-                descripcion:{
-                    alias: 'd',
-                    demand: true,
-                    desc: 'Descripción de la tarea por hacer'
-                }
+                descripcion
             })
             .command('crear','Crea las tareas por hacer',{
-                descripcion:{
-                    alias: 'd',
-                    demand: true,
-                    desc: 'Descripción de la tarea por hacer'
-                    
-                }
-
+                descripcion
             })
             .command('Actualizar','Actualiza e estado completo de una tarea',{
-                descripcion:{
-                    alias: 'd',
-                    demand: true,
-                    desc: 'Descripción de la tarea por hacer'
-                    
-                },
-                completado:{
-                    alias: 'c',
-                    default: true,
-                    desc: 'Marca como completado o pendiente la tarea por hacer'
-                }
+                descripcion,
+                completado
             })
             .help()
             .argv;
